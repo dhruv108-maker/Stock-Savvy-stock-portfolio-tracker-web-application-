1ca17eebfb1598e140f0369566c4726c230bc088
DV.1000 <135990735+dhruv108-maker@users.noreply.github.com>
2024-06-28 16:55:54 +0530

    app.py

--- stock_savvy(stock portfoilio tracker web application)/app.py
+++ stock_savvy(stock portfoilio tracker web application)/app.py
@@ -1,60 +1,16 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 import yfinance as yf
 from pymongo import MongoClient
-import binascii
 import pandas as pd
-import csv
-import io
+from pandas import DataFrame
+from methods import get_stock_data, data, namedata, get_stock_prices, get_current_stock_price, mean
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key' 
+app.secret_key = 'your_secret_key'
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client['clients']
 collection = db['client']
-
-def get_stock_data(symbol, duration):
-    try:
-        stock = yf.Ticker(symbol)
-        hist = stock.history(period=duration)
-        return hist
-    except Exception as e:
-        print("Error fetching data:", e)
-        return None
-
-def data():
-    stock = pd.read_csv("stock_info.csv")
-    symbols = stock['symbol'].tolist()
-    return symbols
-
-def namedata():
-    stock_dict = {}
-    stock = pd.read_csv("stock_info.csv")
-    symbols = stock['symbol'].tolist()
-    names = stock['Name'].tolist()
-    stock_dict = {}
-    for i in range(len(symbols)):
-        stock_dict[names[i]] = symbols[i]
-    return stock_dict
-
-def get_stock_prices(symbol):
-    try:
-        stock = yf.Ticker(symbol)
-        data = stock.history(period="1d")
-        prices = data['Close'].tolist()
-        return prices
-    except Exception as e:
-        print("Error fetching stock prices:", e)
-        return None
-
-def get_current_stock_price(symbol):
-    try:
-        stock = yf.Ticker(symbol)
-        current_price = stock.history(period="1d")['Close'].iloc[-1]  # Get the last closing price
-        return current_price
-    except Exception as e:
-        print("Error fetching data:", e)
-        return None
 
 @app.route('/')
 def index():
@@ -65,7 +21,7 @@ def submit():
     name = request.form['username']
     email = request.form['email']
     password = request.form['password']
-    
+
     # Check if user already exists
     user = collection.find_one({"email": email})
     if user:
@@ -82,7 +38,8 @@ def submit():
     flash("Sign up successful.")
     return redirect(url_for('index'))
 
-@app.route('/index2')#decoraters
+
+@app.route('/index2')  # decoraters
 def index2():
     symbols = data()
     stock_dict = namedata()
@@ -90,7 +47,8 @@ def index2():
         symbol = request.form['symbol'].upper()
     elif request.method == 'GET':
         symbol = request.args.get('symbol', '').upper()
-    return render_template('index2.html', symbols = symbols, stock_dict = stock_dict)
+    return render_template('index2.html', symbols=symbols, stock_dict=stock_dict)
+
 
 @app.route('/plot', methods=['GET', 'POST'])
 def plot():
@@ -100,29 +58,29 @@ def plot():
     elif request.method == 'GET':
         symbol = request.args.get('symbol', '').upper()
         duration = request.args.get('duration', '1y')  # Default to 1 year if not provided
-    
+
     data = get_stock_data(symbol, duration)
     if data is not None:
         dates = data.index.strftime('%Y-%m-%d').tolist()
         prices = data['Close'].tolist()
-        compare_price = prices[-2]
+        compare_price = mean(prices)
         latest_price = data['Close'].tolist()[-1]
-        if(latest_price<100):
-            pe=latest_price/10
-        elif(1000>latest_price>100):
-            pe=latest_price/100
-        elif(10000>latest_price>1000):
-            pe=latest_price/1000
-        latest_price="{:.2f}".format(latest_price)
-        pe="{:.2f}".format(pe)
-        highest_price="{:.2f}".format(max(prices))
-        open_price=data['Close'].tolist()[-2]
-        open_price="{:.2f}".format(open_price)
-        return jsonify(success=True, dates=dates, prices=prices, latest_price=latest_price, pe=pe, highest_price=highest_price, open_price=open_price, compare_price=compare_price)
+        if (latest_price < 100):
+            pe = latest_price / 10
+        elif (1000 > latest_price > 100):
+            pe = latest_price / 100
+        elif (10000 > latest_price > 1000):
+            pe = latest_price / 1000
+        latest_price = "{:.2f}".format(latest_price)
+        pe = "{:.2f}".format(pe)
+        highest_price = "{:.2f}".format(max(prices))
+        open_price = data['Close'].tolist()[-2]
+        open_price = "{:.2f}".format(open_price)
+        return jsonify(success=True, dates=dates, prices=prices, latest_price=latest_price, pe=pe,
+                       highest_price=highest_price, open_price=open_price, compare_price=compare_price)
     else:
-        return jsonify(success=False, error="Error fetching stock data",latest_price="101*", pe="101*")
+        return jsonify(success=False, error="Error fetching stock data", latest_price="101*", pe="101*")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
